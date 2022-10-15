b18043e2be7d491b359ee45121819eebe0f3d89c
VLSLV333 <v.sherstyk@gmail.com>
2022-10-15 16:34:41 +0000

    ger ready for a new adventure

---
+++ app.js
@@ -0,0 +1,19 @@
+const appRoot = document.getElementById('app-root');
+
+/*
+write your code here
+
+list of all regions
+externalService.getRegionsList();
+list of all languages
+externalService.getLanguagesList();
+get countries list by language
+externalService.getCountryListByLanguage()
+get countries list by region
+externalService.getCountryListByRegion()
+*/
+
+console.log(externalService.getRegionsList());
+console.log(externalService.getLanguagesList());
+console.log(externalService.getCountryListByLanguage());
+console.log(externalService.getCountryListByRegion());
